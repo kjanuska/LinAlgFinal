d2f0f257fb3c233c32ec334c3e1151798aebc11b
Kipras Januska <kiprasjanuska0@gmail.com>
2021-12-06 16:56:47 -0500

    Add rotation functions

--- matrix_gen.py
+++ matrix_gen.py
@@ -1,7 +1,12 @@
 import numpy as np
 import turtle
+import time
 
-camera_dist = 1000
+camera_dist = 300
+screen = turtle.Screen()
+screen.tracer(0)
+t = turtle.Turtle(visible=False)
+t.speed(0)
 
 def projcoords(coords):
     projmatrix = [[1, 0, 0, 0],
@@ -15,6 +20,28 @@ def projcoords(coords):
         projected.append(temp)
     return projected
     
+def rotateY(coords, rad):
+    rotmatrix = [[1, 0, 0, 0],                 
+                 [0, np.cos(rad), -np.sin(rad), 0],
+                 [0, np.sin(rad), np.cos(rad), 0],
+                 [0, 0, 0, 1]]
+    rotated = []
+    for c in coords:
+        temp = np.matmul(rotmatrix, c)
+        rotated.append(temp)
+    return rotated
+
+def rotateX(coords, rad):
+    rotmatrix = [[np.cos(rad), 0, -np.sin(rad), 0],
+                 [0, 1, 0, 0],
+                 [np.sin(rad), 0, np.cos(rad), 0],
+                 [0, 0, 0, 1]]
+    rotated = []
+    for c in coords:
+        temp = np.matmul(rotmatrix, c)
+        rotated.append(temp)
+    return rotated
+
 
 def make_cube():
     length = 100
@@ -30,16 +57,52 @@ def make_cube():
     ]
     for v in vertices:
         v = np.transpose(v)
-    vertices = projcoords(vertices)
-    draw_cube(vertices)
+    return vertices
 
 def draw_cube(coords):
-    t = turtle.Turtle()
     t.penup()
-    for c in coords:
-        t.goto(c[0], c[1])
-        t.dot(20)
-    while (True):
-        continue
+    # draw the vertices
+    # for c in coords:
+    #     t.goto(c[0], c[1])
+    #     t.dot(20)
+    
+    # draw the edges in a certain order
+    # front square
+    t.goto(coords[0][0], coords[0][1])
+    t.pendown()
+    t.goto(coords[3][0], coords[3][1])
+    t.goto(coords[6][0], coords[6][1])
+    t.goto(coords[2][0], coords[2][1])
+    t.goto(coords[0][0], coords[0][1])
+    # back square
+    t.goto(coords[1][0], coords[1][1])
+    t.goto(coords[5][0], coords[5][1])
+    t.goto(coords[7][0], coords[7][1])
+    t.goto(coords[4][0], coords[4][1])
+    t.goto(coords[1][0], coords[1][1])
+    t.penup()
+    t.goto(coords[2][0], coords[2][1])
+    t.pendown()
+    t.goto(coords[4][0], coords[4][1])
+    t.penup()
+    t.goto(coords[3][0], coords[3][1])
+    t.pendown()
+    t.goto(coords[5][0], coords[5][1])
+    t.penup()
+    t.goto(coords[6][0], coords[6][1])
+    t.pendown()
+    t.goto(coords[7][0], coords[7][1])
 
-make_cube()
+    turtle.update()
+
+def animate():
+    vertices = make_cube()
+    global camera_dist
+    while(True):
+        vertices = rotateX(vertices, 0.01)
+        vertices = rotateY(vertices, 0.01)
+        draw_cube(projcoords(vertices))
+        time.sleep(0.05)
+        t.clear()
+
+animate()
